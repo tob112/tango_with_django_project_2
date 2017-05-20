ffe93e7b0189f084ffdc7a6ed6877503807e1f38
tobi <tolanger94@gmail.com>
2017-05-20 04:34:02 -0700

    finished chapter 4

---
+++ rango/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+from views import *
+
+
+urlpatterns = patterns('',
+                       url(r'^$', index, name='index'),
+                       url(r'^/about', about, name='page'),
+                       )
+
---
+++ rango/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+
+def index(request):
+    context = RequestContext(request=request)
+
+    context_dict = {'boldmessage': "I am a bold font from the context"}
+
+    return render_to_response('rango/index.html', context_dict, context)
+
+
+
+def about(request):
+   context = RequestContext(request=request)
+
+   return render_to_response('rango/about.html', context)
+
