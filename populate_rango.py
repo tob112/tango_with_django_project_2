fcb30c6f556383a2548a68577c9bc1b215ad0098
tobi <tolanger94@gmail.com>
2017-05-21 08:35:06 -0700

    finished chapter 6

--- populate_rango.py
+++ populate_rango.py
@@ -2,35 +2,42 @@ import os
 
 
 def populate():
-    python_cat = add_cat('Python',128,64)
+    python_cat = add_cat('Python', 128, 64)
 
     add_page(cat=python_cat,
              title="Official Python Tutorial",
-             url="http://docs.python.org/2/tutorial/")
+             url="http://docs.python.org/2/tutorial/",
+             views=423)
     add_page(cat=python_cat,
              title="How to Think like a Computer Scientist",
-             url="http://www.greenteapress.com/thinkpython/")
+             url="http://www.greenteapress.com/thinkpython/",
+             views=32)
     add_page(cat=python_cat,
              title="Learn Python in 10 Minutes",
-             url="http://www.korokithakis.net/tutorials/python/")
-    django_cat = add_cat("Django",64,32)
+             url="http://www.korokithakis.net/tutorials/python/",
+             views=321)
+    django_cat = add_cat("Django", 64, 32)
     add_page(cat=django_cat,
              title="Official Django Tutorial",
-             url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
+             url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/",
+             views=234)
     add_page(cat=django_cat,
              title="Django Rocks",
-             url="http://www.djangorocks.com/")
+             url="http://www.djangorocks.com/",
+             views=213)
     add_page(cat=django_cat,
              title="How to Tango with Django",
-             url="http://www.tangowithdjango.com/")
-    frame_cat = add_cat("Other Frameworks",32,16)
+             url="http://www.tangowithdjango.com/",
+             views=2341)
+    frame_cat = add_cat("Other Frameworks", 32, 16)
     add_page(cat=frame_cat,
              title="Bottle",
-             url="http://bottlepy.org/docs/dev/")
+             url="http://bottlepy.org/docs/dev/",
+             views=433)
 
     add_page(cat=frame_cat,
              title="Flask",
-             url="http://flask.pocoo.org")
+             url="http://flask.pocoo.org", views=123)
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
@@ -42,8 +49,8 @@ def add_page(cat, title, url, views=0):
     return p
 
 
-def add_cat(name,views,likes):
-    c = Category.objects.get_or_create(name=name,views=views,likes=likes)[0]
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     return c
 
 
--- rango/urls.py
+++ rango/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import patterns, url
 from views import *
 
-
 urlpatterns = patterns('',
                        url(r'^$', index, name='index'),
-                       url(r'^/about', about, name='page'),
+                       url(r'^/about$', about, name='page'),
+                       url(r'category/(?P<category_name_url>\w+)/$', category, name='category'),
                        )
-
