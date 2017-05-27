a8973eabff486c386d4c6ef6a8657854f088f7e8
tobi <tolanger94@gmail.com>
2017-05-27 05:27:30 -0700

    chapter 11

--- rango/forms.py
+++ rango/forms.py
@@ -38,7 +38,9 @@ class PageForm(forms.ModelForm):
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(help_text='Please enter a username')
+    email = forms.CharField(help_text='Please enter a email')
+    password = forms.CharField(widget=forms.PasswordInput(), help_text='Please enter a password')
 
     class Meta:
         model = User
@@ -46,6 +48,9 @@ class UserForm(forms.ModelForm):
 
 
 class UserProfileForm(forms.ModelForm):
+    website = forms.URLField(help_text='Please enter a website.', required=False)
+    picture = forms.ImageField(help_text='Select a profile image to upload.', required=False)
+
     class Meta:
         model = UserProfile
 
